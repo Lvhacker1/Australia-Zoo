8eb764814b9817dff1a9ffda604015570d328278
Lvhacker1 <lavasha2@hotmail.com>
2025-06-02 22:40:55 +0200

    small fix

--- src/components/PopUp/index.jsx
+++ src/components/PopUp/index.jsx
@@ -1,9 +1,11 @@
+import Header from "../Header"
 import styles from "./popUp.module.css"
 
 
 const PopUp = ({content, onClose}) => {
     return (
         <div className={styles.popUp}>
+            <Header />
             <button className={styles.closeButton} onClick={onClose}>X</button>
             <div className={styles.popUpContent}>
                 {content}
